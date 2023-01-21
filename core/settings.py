caf0bce190b2b5cda7cbb3ae2d4e3424c29c22e6
Preetam Raj <iitianpreetam@gmail.com>
2023-01-21 14:58:12 +0530

    deployement ready setup

--- core/settings.py
+++ core/settings.py
@@ -139,6 +139,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
